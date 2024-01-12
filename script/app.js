9a5df1e32e17d35c25123c3f49e3f7e9ff1ce848
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-01-12 21:06:28 +0100

    menu styles and JS

--- script/app.js
+++ script/app.js
@@ -1,8 +1,17 @@
 const nav = document.getElementById('nav');
 const menu = document.querySelector('#hamburgerMenu');
+const links = document.querySelectorAll('.link');
 
 menu.addEventListener('click', () => {
     nav.classList.toggle('menu-active');
     menu.classList.toggle('fi-align-justify');
     menu.classList.toggle('fi-arrow-left');
 });
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('menu-active');
+        menu.classList.toggle('fi-align-justify');
+        menu.classList.toggle('fi-arrow-left');
+    })
+})
